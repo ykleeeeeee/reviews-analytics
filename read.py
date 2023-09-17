7790b0a1848fefc4dcae1a5c2c36a878dda4b25c
Yklee <yklee523@gail.com>
2023-09-17 20:16:36 +0800

    add filtering function-2

--- read.py
+++ read.py
@@ -35,3 +35,11 @@ for d in data:
 print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
 print(new[1])
+
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆提到good')
+print(good[0])
